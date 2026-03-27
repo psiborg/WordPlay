f759eaf2114424b9cabb79f6b5957b30d060c565
Jim Ing <jim.ing@gmail.com>
2026-03-27 02:55:08 -0400

    Fixed scoring tables

--- app.js
+++ app.js
@@ -6,15 +6,15 @@
 
 /* -- SCORING TABLES --------------------------------------- */
 const SCORES_CROSSPLAY = {
-  A:1, B:3, C:3, D:2, E:1, F:4, G:2, H:4, I:1, J:8,
-  K:5, L:1, M:3, N:1, O:1, P:3, Q:10,R:1, S:1, T:1,
-  U:1, V:4, W:4, X:8, Y:4, Z:10
+  A:1, B:4, C:3, D:2, E:1, F:4, G:4, H:3, I:1, J:10,
+  K:6, L:2, M:3, N:1, O:1, P:3, Q:10,R:1, S:1, T:1,
+  U:2, V:6, W:5, X:8, Y:4, Z:10
 };
 
 const SCORES_SCRABBLE = {
-  A:1, B:4, C:4, D:3, E:1, F:5, G:4, H:5, I:1, J:8,
-  K:6, L:2, M:4, N:2, O:1, P:4, Q:10,R:2, S:2, T:2,
-  U:2, V:6, W:5, X:8, Y:5, Z:10
+  A:1, B:3, C:3, D:2, E:1, F:4, G:2, H:4, I:1, J:8,
+  K:5, L:1, M:3, N:1, O:1, P:3, Q:10,R:1, S:1, T:1,
+  U:1, V:4, W:4, X:8, Y:4, Z:10
 };
 
 const SCORING_MODES = {
